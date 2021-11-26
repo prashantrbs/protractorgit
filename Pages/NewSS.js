1d140044f9551c60fd3457bcfdfd18f5f5c23f48
Prashanth A <prashanth.gs2016@hotmail.com>
2021-11-26 14:29:25 +0530

    git and jenkin configaration

--- Pages/NewSS.js
+++ Pages/NewSS.js
@@ -4,13 +4,9 @@ describe("Test SUit", function()
 {
     it("newone", function()
     {
-        //browser.waitForAngularEnabled(false);
+        browser.waitForAngularEnabled(false);
         browser.get("https://www.globalsqa.com/angularJs-protractor/Multiform/#/form/profile");
         browser.sleep();
-        browser.element(by.model("formData.name")).sendKeys("asdf");
-        browser.element(by.model("formData.email")).sendKeys("sad@sd.com");
-        browser.element(by.xpath("//a[@ui-sref='form.interests']")).click();
-        browser.sleep(5000);
         
     });
 });
